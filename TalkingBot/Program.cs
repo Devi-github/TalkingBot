64c4eb422d4b554318dd30813a8ca8844d2b2c55
IgorCompanyName <piuuttri@gmail.com>
2023-04-07 17:19:00 +0200

    Created config system

--- TalkingBot/Program.cs
+++ TalkingBot/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace TalkingBot
+﻿using Discord.Interactions;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace TalkingBot
 {
+    internal struct TalkingBotConfig
+    {
+        public string Token { get; set; }
+    }
     internal class Program
     {
-        static void Main(string[] args)
+        public static Task Main(string[] args) => MainAsync(args);
+
+        private static async Task MainAsync(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string cnfpath = "Config.json";
+            string jsonconfig = "";
+            if(args.Length > 1) 
+            {
+                int idx = Array.FindIndex(args, 0, args.Length, str => str == "-C");
+
+                try
+                {
+                    if (idx != -1) cnfpath = args[idx + 1];
+                } catch(ArgumentOutOfRangeException)
+                {
+                    Console.Error.WriteLine("Expected string after '-C'. Got null");
+                    Environment.Exit(-1);
+                }
+            }
+            if(!File.Exists(cnfpath))
+            {
+                await CreateDefaultConfig(cnfpath);
+            }
+            jsonconfig = File.ReadAllText(cnfpath);
+            TalkingBotConfig config = JsonConvert.DeserializeObject<TalkingBotConfig>(jsonconfig)!;
+
+            
+        }
+        private static async Task CreateDefaultConfig(string cnfpath)
+        {
+            TalkingBotConfig config = new();
+
+            Console.WriteLine("Config not found at: {0}\nCreating new one...", cnfpath);
+            Console.Write("Enter application token: ");
+            string? token = Console.ReadLine();
+            if(string.IsNullOrEmpty(token))
+            {
+                Console.Error.WriteLine("Token is not specified!");
+                Environment.Exit(-1);
+            }
+            config.Token = token;
+
+            using(StreamWriter sw = new(cnfpath))
+            {
+                await sw.WriteAsync(JsonConvert.SerializeObject(config));
+            }
         }
     }
 }
