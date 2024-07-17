1ee52821f0df6b9a0ae84d892a9311f9d858f2aa
pi4erd <piuuttri@gmail.com>
2024-07-17 19:16:54 +0200

    clean: dependencies moved to primary constructor

--- TalkingBotLib/Services/CommandHandlerService.cs
+++ TalkingBotLib/Services/CommandHandlerService.cs
@@ -9,56 +9,47 @@ using TalkingBot.Modules;
 
 namespace TalkingBot.Services;
 
-public class CommandHandlerService {
-    private readonly InteractionService _interactions;
-    private readonly DiscordSocketClient _client;
-    private readonly ILogger<InteractionService> _logger;
-
-    public CommandHandlerService() {
-        _interactions = ServiceManager.GetService<InteractionService>();
-        _client = ServiceManager.GetService<DiscordSocketClient>();
-        _logger = ServiceManager.GetService<ILogger<InteractionService>>();
-    }
-
+public class CommandHandlerService(
+    InteractionService interactions,
+    DiscordSocketClient client,
+    ILogger<CommandHandlerService> logger
+) {
     ~CommandHandlerService() {
-        _interactions.Dispose();
+        interactions.Dispose();
     }
 
     public async Task InitializeAsync() {
-        // IEnumerable<ModuleInfo> modules = 
-        //     await _interactions.AddModulesAsync(Assembly.GetEntryAssembly(), ServiceManager.ServiceProvider);
-        
-        await _interactions.AddModuleAsync<BotModule>(ServiceManager.ServiceProvider);
-        await _interactions.AddModuleAsync<AudioModule>(ServiceManager.ServiceProvider);
-        await _interactions.AddModuleAsync<ButtonHandlerModule>(ServiceManager.ServiceProvider);
+        await interactions.AddModuleAsync<BotModule>(ServiceManager.ServiceProvider);
+        await interactions.AddModuleAsync<AudioModule>(ServiceManager.ServiceProvider);
+        await interactions.AddModuleAsync<ButtonHandlerModule>(ServiceManager.ServiceProvider);
 
-        _logger.LogInformation("Loaded interaction modules: {} modules", _interactions.Modules.Count);
+        logger.LogInformation("Loaded interaction modules: {} modules", interactions.Modules.Count);
 
-        _client.Ready += async () => {
+        client.Ready += async () => {
             Stopwatch sw = new();
             foreach(var guild in ServiceManager.GetService<TalkingBotConfig>().Guilds!) {
                 sw.Restart();
-                await _interactions.RegisterCommandsToGuildAsync(guild, true);
+                await interactions.RegisterCommandsToGuildAsync(guild, true);
                 sw.Stop();
 
-                _logger.LogInformation("Registered commands for guild: {}. {} seconds elapsed.",
-                    _client.GetGuild(guild).Name, sw.Elapsed.TotalSeconds
+                logger.LogInformation("Registered commands for guild: {}. {} seconds elapsed.",
+                    client.GetGuild(guild).Name, sw.Elapsed.TotalSeconds
                 );
             }
         };
-        _client.ButtonExecuted += ButtonExecutedAsync;
-        _client.InteractionCreated += InteractionExecutedAsync;
+        client.ButtonExecuted += ButtonExecutedAsync;
+        client.InteractionCreated += InteractionExecutedAsync;
     }
     
     public async Task ButtonExecutedAsync(SocketMessageComponent interaction) {
-        var ctx = new SocketInteractionContext<SocketMessageComponent>(_client, interaction);
-        await _interactions.ExecuteCommandAsync(ctx, ServiceManager.ServiceProvider);
+        var ctx = new SocketInteractionContext<SocketMessageComponent>(client, interaction);
+        await interactions.ExecuteCommandAsync(ctx, ServiceManager.ServiceProvider);
     }
 
     public async Task InteractionExecutedAsync(SocketInteraction interaction) {
         try {
-            var context = new SocketInteractionContext(_client, interaction);
-            var result = await _interactions.ExecuteCommandAsync(context, ServiceManager.ServiceProvider);
+            var context = new SocketInteractionContext(client, interaction);
+            var result = await interactions.ExecuteCommandAsync(context, ServiceManager.ServiceProvider);
 
             if(!result.IsSuccess) {
                 await context.Channel.SendMessageAsync(result.ToString(), flags: MessageFlags.Ephemeral);
